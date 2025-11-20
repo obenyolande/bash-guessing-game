3a09eecf696483038ce79c0e2082788fb91643de
yolande oben <obenyolande@gmail.com>
2025-11-20 18:30:21 +0100

    Fix a bug

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+x=$(( 0 + random % (20 -0 + 1) ))
+
+echo "================================="
+      Guess a number between 0 to 30
+echo "=================================="
+read -p "Enter value guessed: " y
+
+if [[ $y -eq $x ]]; then
+echo "you Win"
+elif [[ $y -lt $x ]]; then
+echo "you Loose"
+else
+echo "you're below the win number"
+fi
