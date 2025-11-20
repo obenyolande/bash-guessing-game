16dab648fe0db3041b814dc4af8a9bc0b1dbdd8e
yolande oben <obenyolande@gmail.com>
2025-11-20 17:08:32 +0100

    i created a new branched called feature/guessing game

--- script.sh
+++ script.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+x=$(( 0 + random % (20 -0 + 1) ))
+
+echo "================================="
+      Guess a number between 0 to 30
+echo "=================================="
+read -p "Enter value guessed: " y
+
+if [[ $y -eq $x ]]; then
+echo "you Win"
+elif [[ $y -lt $x ]]; then
+echo "you Loose"
+echo "you're below the win number"
+fi
